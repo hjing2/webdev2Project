492e3e06419b89c1f8899ecfef9af829f35b089c
hjing2 <150513235+hjing2@users.noreply.github.com>
2024-03-18 15:42:43 -0500

    Header

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+
+/*******w******** 
+    
+    Name: Haodan Jing
+    Date: Jan 29th, 2024
+    Description: This is the edit page to update the existing blog title and content. It include update and delete function, and will not update the blog if there's no character in the title or content.
+
+****************/
+?>
+
+<div id="header">
+    <h1><a href="index.php">FurEver Winnipeg</a></h1>
+</div> 
+<ul id="menu">
+    <li><a href="index.php" class="active">Home</a></li>
+    <li><a href="post.php">New Pets Available</a></li>
+</ul>  
